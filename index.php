fa9fffeb53948c432aa88f1f7ec248bd83557edf
Wissididom <30803034+Wissididom@users.noreply.github.com>
2023-01-23 16:31:30 +0100

    Add ACK `PING` message handling

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-echo "Test";
+$jsonReq = json_decode(file_get_contents('php://input'), true);
+if ($jsonReq['type'] == 1) {
+    $responseObj = array(
+        'type' => 1
+    );
+    echo json_encode($responseObj);
+}
 ?>

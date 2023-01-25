eb3f31b98ae966fd52204d369c5db9975042a91f
Wissididom <30803034+Wissididom@users.noreply.github.com>
2023-01-25 09:34:03 +0100

    Added Null Check to $jsonReq

--- api/index.php
+++ api/index.php
@@ -1,5 +1,9 @@
 <?php
 $jsonReq = json_decode(file_get_contents('php://input'), true);
+if (is_null($jsonReq)) {
+    http_response_code(400);
+    echo "Invalid Form Body";
+}
 if ($jsonReq['type'] == 1) {
     $responseObj = array(
         'type' => 1
